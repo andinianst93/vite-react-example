44bea522a544aae87d76f5bd2f1dc48be328a4fe
andinianst93 <andinidev93@gmail.com>
2024-10-11 09:55:17 +0700

    preview ok

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import logo from "../../public/morphius-logo.png";
+import logo from "/morphius-logo.png";
 
 export default function Navigation() {
   return (
